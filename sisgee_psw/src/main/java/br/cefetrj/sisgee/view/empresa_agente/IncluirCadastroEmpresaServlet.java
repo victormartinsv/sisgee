cfe9e7532a3390c7d9b809f1526cac3029fcd333
Natalia <Natalia@192.168.11.17>
2017-11-29 17:54:11 -0200

    Alteração nas validações de cadastro de empresa. Nomes repetidos, cnpj repetido. I18N do servlet incluir cadastro de empresa

--- sisgee_psw/src/main/java/br/cefetrj/sisgee/view/empresa_agente/IncluirCadastroEmpresaServlet.java
+++ sisgee_psw/src/main/java/br/cefetrj/sisgee/view/empresa_agente/IncluirCadastroEmpresaServlet.java
@@ -1,6 +1,8 @@
 package br.cefetrj.sisgee.view.empresa_agente;
 
 import java.io.IOException;
+import java.util.ResourceBundle;
+import java.util.Locale;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -14,6 +16,7 @@ import br.cefetrj.sisgee.control.AgenteIntegracaoServices;
 import br.cefetrj.sisgee.control.EmpresaServices;
 import br.cefetrj.sisgee.model.entity.AgenteIntegracao;
 import br.cefetrj.sisgee.model.entity.Empresa;
+import br.cefetrj.sisgee.view.utils.ServletUtils;
 
 @WebServlet("/IncluirCadastroEmpresaServlet")
 public class IncluirCadastroEmpresaServlet extends HttpServlet {
@@ -23,6 +26,8 @@ public class IncluirCadastroEmpresaServlet extends HttpServlet {
 	protected void service(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 
+		Locale locale = ServletUtils.getLocale(request);
+		ResourceBundle messages = ResourceBundle.getBundle("Messages", locale);
 		String cnpjEmpresa = request.getParameter("cnpjEmpresa");
 		String nomeEmpresa = request.getParameter("nomeEmpresa");
 		String agenteIntegracao = request.getParameter("agenteIntegracao");
@@ -38,13 +43,13 @@ public class IncluirCadastroEmpresaServlet extends HttpServlet {
 			Logger lg = Logger.getLogger(IncluirCadastroEmpresaServlet.class);
 			try {
 				EmpresaServices.incluirEmpresa(empresa);
-				msg = "Agente Integração cadastrada com sucesso.";
+				msg = messages.getString("br.cefetrj.sisgee.incluir_cadastro_empresa_servlet.msg_empresa_cadastrada");
 				request.setAttribute("msg", msg);
 				lg.info(msg);
 				request.getRequestDispatcher("/index.jsp").forward(request, response);
 
 			} catch (Exception e) {
-				msg = "Ocorreu um erro inesperado ao tentar cadastrar a Empresa. Tente novamente ou contate o suporte caso o erro persista.";
+				msg = messages.getString("br.cefetrj.sisgee.incluir_cadastro_empresa_servlet.msg_ocorreu_erro");
 				request.setAttribute("msg", msg);
 				lg.error("Exception ao tentar inserir uma Empresa", e);
 				request.getRequestDispatcher("ValidaCadastroEmpresaServlet").forward(request, response);
@@ -56,13 +61,13 @@ public class IncluirCadastroEmpresaServlet extends HttpServlet {
 			Logger lg = Logger.getLogger(IncluirCadastroEmpresaServlet.class);
 			try {
 				AgenteIntegracaoServices.incluirAgenteIntegracao(agente);
-				msg = "Empresa cadastrada com sucesso.";
+				msg = messages.getString("br.cefetrj.sisgee.incluir_cadastro_empresa_servlet.msg_agente_cadastrado");
 				request.setAttribute("msg", msg);
 				lg.info(msg);
 				request.getRequestDispatcher("/index.jsp").forward(request, response);
 
 			} catch (Exception e) {
-				msg = "Ocorreu um erro inesperado ao tentar cadastrar a Empresa. Tente novamente ou contate o suporte caso o erro persista.";
+				msg = messages.getString("br.cefetrj.sisgee.incluir_cadastro_empresa_servlet.msg_ocorreu_erro");
 				request.setAttribute("msg", msg);
 				lg.error("Exception ao tentar inserir uma Empresa", e);
 				request.getRequestDispatcher("ValidaCadastroEmpresaServlet").forward(request, response);

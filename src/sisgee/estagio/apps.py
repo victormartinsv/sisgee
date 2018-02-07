43b13f85d1b0e790fa1b004b5c7878e36150b5e3
Diogo <diogosmendonca@gmail.com>
2018-02-07 15:39:42 -0200

    Remoção de fontes em python e testes antigos. Inclusão de docs dos testes.

--- src/sisgee/estagio/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class EstagioConfig(AppConfig):
-    name = 'estagio'
--- src/sisgee/estagio/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-from .validators import validate_formato
-
-
-class ImportForm(forms.Form):
-    file = forms.FileField(label='Arquivo', validators=[validate_formato])
